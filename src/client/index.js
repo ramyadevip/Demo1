e3c3adf5ffedef4b2574cc17259070645e81084d
Ramyadevi <ramyakavi91@gmail.com>
2020-02-12 21:06:27 +0000

    up down counter

--- src/client/index.js
+++ src/client/index.js
@@ -1,9 +1,30 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { render } from 'react-dom';
 
-class App extends Component {
+class App extends React.Component {
+  state = {
+    count : 20,
+  };
+  increment = () => {
+    const currentValue = this.state.count;
+    this.setState ({
+      // eslint-disable-next-line no-magic-numbers
+      count : currentValue + 1,
+    });
+  }
+  decrement = () => {
+    const currentValue = this.state.count;
+    this.setState ({
+      // eslint-disable-next-line no-magic-numbers
+      count : currentValue - 1,
+    });
+  }
   render() {
-    return <h1>Hello, World</h1>;
+    return <div>
+      <h1>{this.state.count}</h1>
+      <button onClick={this.increment}>+</button>
+      <button onClick={this.decrement}>-</button>
+    </div>;
   }
 }
 
